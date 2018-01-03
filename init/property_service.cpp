a1d8acd4e89ad4ac8e9b15b24f85eb9d96c2819c
Shilin Victor <chrono.monochrome@gmail.com>
2018-01-03 11:07:35 +0300

    init: allow selinux to be disabled
    Change-Id: Ic1da3edaa5baf40cbfcab947d935082a1659d97e

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -77,6 +77,8 @@ void property_init() {
 }
 
 static bool check_mac_perms(const std::string& name, char* sctx, struct ucred* cr) {
+    if (is_selinux_enabled() <= 0)
+        return 1;
 
     if (!sctx) {
       return false;
