33999eedde039cf8c39b5c95a93853729822a252
Shilin Victor <chrono.monochrome@gmail.com>
2018-02-19 09:58:58 +0300

    liblog: only apply -std=gnu99 flag to C files

--- liblog/Android.mk
+++ liblog/Android.mk
@@ -78,13 +78,15 @@ include $(BUILD_HOST_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := liblog
 LOCAL_SRC_FILES := $(liblog_target_sources)
-LOCAL_CFLAGS := -std=gnu99 $(liblog_cflags)
+LOCAL_CONLYFLAGS := -std=gnu99
+LOCAL_CFLAGS := $(liblog_cflags)
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := liblog_nolto
 LOCAL_SRC_FILES := $(liblog_target_sources)
-LOCAL_CFLAGS := -std=gnu99 $(liblog_cflags)
+LOCAL_CONLYFLAGS := -std=gnu99
+LOCAL_CFLAGS := $(liblog_cflags)
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
